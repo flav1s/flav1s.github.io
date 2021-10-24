c21b28ca99613cb839e5c3c9ee6648b7768292c6
Flávia Rocha <flaviaccruzr@gmail.com>
2021-10-24 11:50:58 -0300

    add eslint

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        speed: 3,
-        strip: true,
-        verbose: true,
-      },
-      imagesPublicPath: "/portfolio/_next/static/images/",
-    },
-  ],
-  {
-    basePath: "/portfolio",
-    assetPrefix: "/portfolio/",
-    env,
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+    };
   },
-]);
+  assetPrefix: !debug ? "https://flav1s.github.io/portfolio/" : "",
+};
