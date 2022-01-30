7dd64cf547575161d88c644aa461dc08a8190e6d
flav1s <flav1s@users.noreply.github.com>
2022-01-30 15:34:25 +0000

    deploy: 5875dbccf52947e3800b5bf59739ea2dd2b885b9

---
+++ _next/static/pzd3VaULC9qNSv8h_Lmpr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/378-5ec02d4488e60798981d.js","static/chunks/pages/index-13a68b8ed13356f3e392.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
