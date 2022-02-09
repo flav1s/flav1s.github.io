8e12e6a62ae0f536b5c7e8aa744b8be647b0fdb5
flav1s <flav1s@users.noreply.github.com>
2022-02-09 13:48:14 +0000

    deploy: b270ae0d1babc6ab69473b434679bb57826091b6

---
+++ _next/static/ejhBHaytHnqZ8qngjWem8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/293-2682a899478d825544dd.js","static/chunks/pages/index-3f86f4e4daa4a1518a27.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
